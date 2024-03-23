c0e4c11e201089ce0848369f091856519dbadc79
JuanFelipeGiraldo <juanfelipe98x@gmail.com>
2024-03-22 19:03:59 -0500

    final Version

--- src/main/java/validator/test/controller/ValidatorController.java
+++ src/main/java/validator/test/controller/ValidatorController.java
@@ -1,7 +1,6 @@
 package validator.test.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,20 +17,7 @@ public class ValidatorController {
         this.csvInformationService = csvInformationService;
     }
 
-    @GetMapping("/validator")
-    public String test() {
-        return "Validator ON";
-    }
-
-
-    @GetMapping("/test/x")
-    public String obtenerDatos() {
-        // Lógica para obtener los datos del servicio 2
-        return "Datos del test";
-
-    }
-
-    @PostMapping("/usuarios/cvs")
+    @PostMapping("/validator/cvs")
     public boolean validatorUser(@RequestBody CsvInformation csvInformation) {
         return this.csvInformationService.validateUser(csvInformation);
     }
